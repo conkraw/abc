2ab2e0d307b159138d58f61ad3a66da4d74ef485
conkraw <conkraw@gmail.com>
2024-10-06 20:53:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -205,21 +205,21 @@ with st.form("airway_form"):
         ett_options = ['', '3.0', '3.5', '4.0', '4.5', '5.0', '5.5', '6.0', '6.5', '7.0', '7.5', '8.0']
         ett_size = st.selectbox("ETT Size", ett_options, key="ett_size")
 
-    st.write("Device Used for Intubation:")
+    st.write("Device:")
 
     # Create a row with checkboxes for device selection
-    cols = st.columns([1, 1, 1, 3])  # Adjust the column widths to suit the content
+    cols = st.columns([1, 1])  # Adjust the column widths to suit the content
     
     with cols[0]:
         laryngoscope = st.checkbox("Laryngoscope", key="laryngoscope")
     
-    with cols[1]:
+    with cols[0]:
         lma = st.checkbox("LMA", key="lma")
     
-    with cols[2]:
+    with cols[1]:
         glidescope = st.checkbox("Glidescope", key="glidescope")
     
-    with cols[3]:
+    with cols[1]:
         other_device = st.text_input("Other (Specify)", key="other_device")
         
     # Timing of Intubation section
