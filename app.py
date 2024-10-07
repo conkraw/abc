df7c35b85df77ec2e56ad617fcefebb71a57402a
conkraw <conkraw@gmail.com>
2024-10-06 21:17:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -212,15 +212,11 @@ with st.form("airway_form"):
 
     with cols[0]:
         st.markdown("")
-        st.markdown("")
         laryngoscope = st.checkbox("Laryngoscope", key="laryngoscope")
-        st.markdown("")
         st.markdown("")
         glidescope = st.checkbox("Glidescope", key="glidescope")
         st.markdown("")
-        st.markdown("")
         lma = st.checkbox("LMA", key="lma")
-        st.markdown("")
         st.markdown("")
         other_device = st.checkbox("Other Device", key="other_device")
     
