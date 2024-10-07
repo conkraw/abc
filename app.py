05f8a68632d1c1f590bce5632eca6266705ed349
conkraw <conkraw@gmail.com>
2024-10-06 21:44:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -222,6 +222,7 @@ with st.form("airway_form"):
     # Column 2: Device dropdowns (Laryngoscope, Glidescope, LMA, Other)
     with cols[1]:
         st.markdown("")
+        st.markdown("")
         st.markdown("**Laryngoscope**")
         #st.text_input("Device #1", value="Laryngoscope", key="laryngoscope_textx", disabled=True)
         #st.text_input("Device #2", value="Glidescope", key="glidescope_textx", disabled=True)
