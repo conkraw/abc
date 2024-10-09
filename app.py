98e9825077c552a80b4f552a08a023e26b25bd09
conkraw <conkraw@gmail.com>
2024-10-08 21:37:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.title("PDF Form Filler")
 
 # Text input for user
 custom_text = st.text_input("Enter text to fill in PDF (e.g., '98%'):")
-field_name = 'date'  # Change this to your desired text input field name
+field_name = 'spo2'  # Change this to your desired text input field name
 
 # File uploader
 uploaded_file = st.file_uploader("Upload PDF", type=["pdf"])
@@ -23,15 +23,12 @@ if uploaded_file is not None:
             annotations = page['/Annots']
             for annot in annotations:
                 annot_obj = annot.get_object()
-                st.write(f"Annotation object: {annot_obj}")  # Print the annotation object
-                st.write(f"Field name: {annot_obj.get('/T')}")  # Print the field name
 
                 # Check if the field name matches
                 if annot_obj.get('/T') == NameObject(field_name):
                     annot_obj.update({
                         NameObject('/V'): TextStringObject(custom_text)  # Set the value
                     })
-                    st.write(f"Updated {field_name} with value: {custom_text}")  # Confirmation message
 
         writer.add_page(page)  # Add the modified page to the writer
 
