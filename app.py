4365467731607f1c0c60aa97d9f19306d3bc34cf
conkraw <conkraw@gmail.com>
2024-10-12 22:00:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -611,7 +611,7 @@ for key, value in default_values.items():
   #        pass
 
 # Front Page Completed Section
-elif st.session_state['section'] == 1:
+if st.session_state['section'] == 1:
     st.title("Front Page Completed")
     
     # Selectbox for front page completion
