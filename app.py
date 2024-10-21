8a4d535e41fefe5d3bb2672750db3c477cde53b8
conkraw <conkraw@gmail.com>
2024-10-21 11:39:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1102,7 +1102,7 @@ elif st.session_state.section == 3:
         custom_order = ['LMA Size 1', 'LMA Size 1.5', 'LMA Size 2', 'LMA Size 2.5', 'LMA Size 3', 'LMA Size 4', 'LMA Size 5']
         lma_details = ["Select an LMA Size"] + [size for size in custom_order if size in lma_details]
         if 'lma_details' in st.session_state and st.session_state['lma_details'] in lma_details:
-          lma_details st.selectbox(
+          lma_details = st.selectbox(
             "LMA Size Details:",
             options = lma_details,
             key="lma_size_display",
